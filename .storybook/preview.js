e38fd44e323a3fba53488029751c0786139a81ec
Houssam Jelliti <jelitihoussam@gmail.com>
2021-11-07 13:03:23 +0100

    build(Storybook): add initial Storybook setup

---
+++ .storybook/preview.js
@@ -0,0 +1,15 @@
+export const parameters = {
+  actions: { argTypesRegex: "^on[A-Z].*" },
+  controls: {
+    matchers: {
+      color: /(background|color)$/i,
+      date: /Date$/,
+    },
+  },
+  a11y: {
+    element: "#root",
+    config: {},
+    options: {},
+    manual: true,
+  }
+}
