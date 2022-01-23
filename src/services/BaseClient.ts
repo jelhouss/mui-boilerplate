8a1b6fbd7aaa5eb74520ac5742bfb917875989bc
Houssam Jelliti <jelitihoussam@gmail.com>
2022-01-23 17:16:20 +0100

    refactor: add BaseClient class as basic Axios wrapper

---
+++ src/services/BaseClient.ts
@@ -0,0 +1,38 @@
+import axios, { AxiosInstance, AxiosRequestConfig } from "axios"
+
+class BaseClient {
+  private accessToken: string | null = null
+
+  protected client: AxiosInstance
+
+  constructor(private configuration: AxiosRequestConfig) {
+    this.client = axios.create(this.configuration)
+
+    this.client.interceptors.request.use(
+      (config) => {
+        if (this.accessToken) {
+          return {
+            ...config,
+            headers: {
+              ...config.headers,
+              Authorization: `Bearer ${this.accessToken}`
+            }
+          }
+        }
+
+        return config
+      },
+      (error) => Promise.reject(error)
+    )
+  }
+
+  public setAccessToken(accessToken: string | null) {
+    this.accessToken = accessToken
+  }
+
+  public getAccessToken(): string | null {
+    return this.accessToken
+  }
+}
+
+export default BaseClient
