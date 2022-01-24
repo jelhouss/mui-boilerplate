4cb8d8324ad9b295d88f274fb8055a6060c6d2ea
Houssam Jelliti <jelitihoussam@gmail.com>
2022-01-24 14:56:08 +0100

    feat: add basic setup of React Query

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,9 +1,23 @@
 import React from "react"
+import { QueryClient, QueryClientProvider } from "react-query"
 
 import PublicLayout from "../pages/public"
 
+const queryCache = new QueryClient({
+  defaultOptions: {
+    queries: {
+      // data will be considered stale (needs to automatically re-fetch) after 5 mins
+      staleTime: 1000 * 60 * 5
+    }
+  }
+})
+
 const App = () => {
-  return <PublicLayout />
+  return (
+    <QueryClientProvider client={queryCache}>
+      <PublicLayout />
+    </QueryClientProvider>
+  )
 }
 
 export default App
