7c646f47c9ff9523dd5a654c66d3adfc253c7895
Houssam Jelliti <jelitihoussam@gmail.com>
2022-01-26 17:37:49 +0100

    test: fix App.test.tsx by asserting that app finished refreshing

--- src/app/App.test.tsx
+++ src/app/App.test.tsx
@@ -1,13 +1,15 @@
-import { render } from "@testing-library/react"
+import { render, screen, waitForElementToBeRemoved } from "@testing-library/react"
 import React from "react"
 import { MemoryRouter } from "react-router-dom"
 
 import App from "./App"
 
-test("should render without crashing", () => {
+test("should render without crashing", async () => {
   render(
     <MemoryRouter>
       <App />
     </MemoryRouter>
   )
+
+  await waitForElementToBeRemoved(() => screen.queryByText(/application is refreshing/i))
 })
