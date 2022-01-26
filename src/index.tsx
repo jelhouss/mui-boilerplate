8ab70a900d0ae27156e9b1ab34296f71fae80237
Houssam Jelliti <jelitihoussam@gmail.com>
2022-01-26 17:23:03 +0100

    chore: prepare MSW request handler to mount before app

--- src/index.tsx
+++ src/index.tsx
@@ -5,20 +5,30 @@ import { BrowserRouter } from "react-router-dom"
 import App from "./app/App"
 import reportWebVitals from "./reportWebVitals"
 
-if (process.env.REACT_APP_OFFLINE) {
-  // eslint-disable-next-line @typescript-eslint/no-var-requires, global-require
-  const worker = require("./mocks/msw/browserMSWSetup").default
-  worker.start()
+// I had an issue when testing offline MSW API with refreshing authentication
+// API endpoint `/api/authenticate/refresh` was not found by MSW
+// see: https://mswjs.io/docs/recipes/deferred-mounting
+function boostrapMSWRegistration() {
+  if (process.env.REACT_APP_OFFLINE) {
+    // eslint-disable-next-line @typescript-eslint/no-var-requires, global-require
+    const worker = require("./mocks/msw/browserMSWSetup").default
+    worker.printHandlers()
+    return worker.start()
+  }
+
+  return Promise.resolve()
 }
 
-ReactDOM.render(
-  <React.StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
-  </React.StrictMode>,
-  document.getElementById("root")
-)
+boostrapMSWRegistration().then(() => {
+  ReactDOM.render(
+    <React.StrictMode>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </React.StrictMode>,
+    document.getElementById("root")
+  )
+})
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
