bf3ea2fc4a9b313242863eee9f3dcbd1c3cb4919
Houssam Jelliti <jelitihoussam@gmail.com>
2023-03-04 14:20:31 +0100

    refactor: remove all files

--- src/mocks/msw/nodeMSWSetup.ts
+++
@@ -1,8 +0,0 @@
-// eslint-disable-next-line import/no-extraneous-dependencies
-import { setupServer } from "msw/node"
-
-import endpoints from "./mswenv"
-
-const server = setupServer(...endpoints)
-
-export default server
