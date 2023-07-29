32bec862d52e42012d0c3cdff2c87b84a31f73e6
Houssam Jelliti <jelitihoussam@gmail.com>
2023-07-29 16:02:32 +0100

    build: upgrade packages

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,19 +1,15 @@
 module.exports = {
   stories: ["../src/**/*.stories.mdx", "../src/**/*.stories.@(js|jsx|ts|tsx)"],
-  addons: [
-    "@storybook/addon-links",
-    "@storybook/addon-essentials",
-    "@storybook/addon-interactions",
-    "@storybook/preset-create-react-app",
-    "@storybook/addon-a11y",
-    "@storybook/addon-coverage"
-  ],
-  framework: "@storybook/react",
-  core: {
-    builder: "@storybook/builder-webpack5"
+  addons: ["@storybook/addon-links", "@storybook/addon-essentials", "@storybook/addon-interactions", "@storybook/preset-create-react-app", "@storybook/addon-a11y", "@storybook/addon-coverage"],
+  framework: {
+    name: "@storybook/react-webpack5",
+    options: {}
   },
   features: {
     interactionsDebugger: true
   },
-  staticDirs: ["../public"]
-}
+  staticDirs: ["../public"],
+  docs: {
+    autodocs: true
+  }
+};
